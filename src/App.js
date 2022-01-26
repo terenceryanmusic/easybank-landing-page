d000f1a3e3ad7a830c0ccfa8d8db15feb629f8d0
TerenceWaldeck <53048309+terenceryanmusic@users.noreply.github.com>
2022-01-26 23:33:44 +0000

    added tailwind browser-router and started homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AppLogo from './img/logo.svg';
+import HomePage from './components/HomePage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div className='min-h-screen bg-gradient-to-br from-slate-700 to-slate-900'>
+				<header className='flex p-10 h-20 bg-gray-100'>
+					<div className='flex items-center'>
+						<img src={AppLogo} alt='' />
+						<nav className='absolute'>
+							<ul>
+								<li></li>
+								<li></li>
+								<li></li>
+								<li></li>
+							</ul>
+						</nav>
+					</div>
+				</header>
+			</div>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='' element='' />
+				<Route path='' element='' />
+				<Route path='' element='' />
+				<Route path='*' element='' />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+	return (
+		<div>
+			<main className='bg-gray-100'>
+				<div className="bg-[url('/img/bg-intro-mobile.svg')]"></div>
+			</main>
+		</div>
+	);
+};
+
+export default HomePage;
